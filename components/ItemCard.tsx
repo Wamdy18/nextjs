c7280e343751d4af978b8dce4902381da903106b
vladimir <qkill322@mail.ru>
2023-11-10 21:15:33 +0500

    ?

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const ItemCard = ({item} : any) => {
   return (
       <div className='item-card'>
-            <Link href={`/items/${item.id}`} className='inline-block mx-2 transition-transform'>
+            <Link href={`/items/${item ? item.id : 1}`} className='inline-block mx-2 transition-transform'>
                 <Image height={300} width={220} src={item.image} alt="item 1" />
                 <div className='mt-2'>{item.label}</div>
                 <div className='my-2'>
